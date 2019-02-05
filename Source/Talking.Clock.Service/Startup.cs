f9780956fef7c2d152d50617cad548e7f5adc1f9
Paulie-Waulie <paul.schoolden@sky.com>
2019-02-05 22:00:25 +0000

    Updaed docker file to run tests and fixed culture in app start up to stop format issues in docker container

--- Source/Talking.Clock.Service/Startup.cs
+++ Source/Talking.Clock.Service/Startup.cs
@@ -1,5 +1,6 @@
 ﻿namespace Talking.Clock.Service
 {
+    using System.Globalization;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Mvc;
@@ -19,6 +20,8 @@
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            CultureInfo.CurrentUICulture = CultureInfo.GetCultureInfo("en-GB");
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddSingleton<IDateService, DateService>();
             services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
